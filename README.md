30d8e763dd44becf0eef4895096243d2b3cb038e
I_NEED_TO_SLEEP\Mikhail <mmaksimov@stetson.edu>
2019-04-15 13:24:38 -0400

    Added orbit controls to the scene and changed texture

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# demo-three.js
+# Demo for three.js
+by Mikhail Maksimov
+
+three.js is a graphics library that builds upon WebGL to make it easier for web-developers create 3D graphics in the browser.
+
+If you want to start using three.js, you will need to download the three.js libraries (which are over 340MB in size). You can put the entire folder in your project folder, or copy only the files that you need from the libraries into a folder in your project. The other option would be to use a package manager like npm to install the three.js module in your project.
+
+All of the configuration and logic for three.js will be done in your own javascript file. I named my file main.js for this demo. Look through the file to see exactly how I created my 3D scene. You will notice that you can rotate around my object while pressing the left mouse button, and pan while holding shift. This functionality is part of OrbitControls.js, which is also part of the libraries.
--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 let scene, camera, renderer;
+let controls;
 let cube;
 
 init();
 animate();
 
+/**
+ * Sets up the 3D scene.
+ */
 function init()
 {
     window.addEventListener( 'resize', onWindowResize, false );
@@ -16,22 +20,36 @@ function init()
     document.body.appendChild( renderer.domElement );
 
     let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    let texture = new THREE.TextureLoader().load( 'textures/dice-1-hi.png' );
-    //{ color: 0x00ff00 }
-    let material = new THREE.MeshBasicMaterial( { map: texture } ); //{ map: texture }
+    let texture = new THREE.TextureLoader().load( 'textures/crate.jpg' );
+    texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+    let material = new THREE.MeshBasicMaterial( { map: texture } );
     cube = new THREE.Mesh( geometry, material );
 
     scene.add( cube );
-    camera.position.z = 5;
+    camera.position.z = 2;
+
+    controls = new THREE.OrbitControls( camera );
+    controls.enableDamping = true;
+    controls.dampingFactor = .1;
+    controls.screenSpacePanning = true;
+    controls.minDistance = 2;
+    controls.maxDistance = 5;
+    controls.rotateSpeed = 0.1;
+    controls.panSpeed = .08;
 }
 
+/**
+ * Updates the scene repeatedly.
+ */
 function animate() {
     requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    controls.update();
     renderer.render( scene, camera );
 }
 
+/**
+ * Resize the renderer to match the browser window size.
+ */
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
